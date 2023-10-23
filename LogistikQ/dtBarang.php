404cb5cccd955cff5b24b7e8a5419cee2da04d8b
michailtjhang <michailaja@gmail.com>
2023-10-23 16:21:07 +0700

    menambahkan table barang

---
+++ LogistikQ/dtBarang.php
@@ -0,0 +1,42 @@
+<?php  
+
+$model =  new barang();
+$data_barang = $model->dataBarang();
+
+?>
+
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-12">
+                        <div class="bg-secondary rounded h-100 p-4">
+                            <h6 class="mb-4">Table Barang</h6>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Kode</th>
+                                            <th scope="col">Kategori</th>
+                                            <th scope="col">Deskripsi</th>
+                                            <th scope="col">Kisaran Harga</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php $id = 1; ?>
+                                    <?php foreach ( $data_barang as $row ) : ?>
+                                        <tr>
+                                            <th scope="row"><?= $id; ?></th>
+                                            <td><?= $row['kode']; ?></td>
+                                            <td><?= $row['kategori']; ?></td>
+                                            <td><?= $row['deskripsi']; ?></td>
+                                            <td><?= $row['kisaran_harga']; ?></td>
+                                        </tr>
+                                    <?php $id++; ?>
+		                            <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
